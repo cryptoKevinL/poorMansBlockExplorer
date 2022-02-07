6dc9e81f1fce866cdcf28092d41ea846b8ec9a64
cyberKevin <>
2022-02-06 20:00:09 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 import * as styles from './index.scss'; 
-const SHA256 = require('js-sha256');
-const port = 4000; //TODO: bootstrap port 4000 is "well known" peer...
-const server = `http://localhost:${port}`;
 const axios = require('axios');
 const ethers = require('ethers');
 require('dotenv').config();
 
 const ALCHEMY_URL = process.env.RINKEBY_URL;
-
-var EC = require('elliptic').ec;
-var ec = new EC('secp256k1');
-const key = ec.genKeyPair();
-//TODO: longer term, we would do this locally, and interface/wallet generate and store the private key
-const publicKey = key.getPublic().encode('hex');
-const privateKey = key.getPrivate().toString('hex');
-//TODO: we need to announce these keys to the network 
 
 function getLatestBlock(){
   axios.post(ALCHEMY_URL, {
@@ -28,22 +17,21 @@ function getLatestBlock(){
   }).then((response) => {
     let blocknum = BigInt(response.data.result.number).toString();
     let transactions = response.data.result.transactions;
-
-    //let data = JSON.stringify(transactions);
-    console.table(transactions);
    
-    // (C) GENERATE TABLE
-    // (C1) CREATE EMPTY TABLE
-    var table = document.createElement("table"), row, cellA, cellB;
+    var table = document.createElement("table"), row, cellA, cellB, cellC, cellD;
     for (let i=0; i<transactions.length; i++){
       // (C2) ROWS & CELLS
       row = table.insertRow();
       cellA = row.insertCell();
       cellB = row.insertCell();
+      cellC = row.insertCell();
+      cellD = row.insertCell();
    
       // (C3) KEY & VALUE
       cellA.innerHTML = "From: ";
       cellB.innerHTML = transactions[i].from;
+      cellC.innerHTML = "To: ";
+      cellD.innerHTML = transactions[i].to;
     }
     
     //append the compiled table to the DOM
